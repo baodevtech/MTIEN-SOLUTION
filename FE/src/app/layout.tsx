281a09c6b7db037f48fd960e6e8b84dd6b5371fd
baodevtech <baodev.tech@gmail.com>
2026-04-18 11:43:45 +0700

    Update layout.tsx

--- FE/src/app/layout.tsx
+++ FE/src/app/layout.tsx
@@ -156,11 +156,22 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         {fbPixelId && fbPixelId !== 'Pixel ID' && (
           <script dangerouslySetInnerHTML={{ __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','${fbPixelId.replace(/'/g, "\\'")}');fbq('track','PageView');` }} />
         )}
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MWC4BFF5');` }} />
       </head>
       <body
         className="bg-white text-slate-800 antialiased selection:bg-blue-100 selection:text-blue-900"
         suppressHydrationWarning
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MWC4BFF5"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         {/* Skip to main content - Accessibility */}
         <a
           href="#main-content"
